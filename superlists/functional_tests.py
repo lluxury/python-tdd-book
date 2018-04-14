87dd35dfdb005d8a34e0d675ed364dd9e2674581
yann <tmp4321@qq.com>
2018-04-14 17:30:39 +0800

    Functional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 import os
 import sys
@@ -13,11 +15,30 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self): 
+        # check out it's homepage
         self.browser.get('http://localhost:8000')
+
+        # notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  
+        header_test = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # enter a to-do item straight away 
+        inputbox = self.brownser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
+        # types "Buy peacock feathers"
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+
+        table = self.browser.find_selement_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+
         self.fail('Finish the test!')
 
-        #other
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')
