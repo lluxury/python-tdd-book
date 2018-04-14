1a75c11ac0154f72e5e3b676233b72451d54aa2a
yann <tmp4321@qq.com>
2018-04-14 21:57:13 +0800

    using assertTemplateUsed

--- lists/tests.py
+++ lists/tests.py
@@ -12,11 +12,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        # respone = home_page(request)
-        html = response.content.decode('utf8')
-        # html = responese.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
-        # self.assertEquual(html, expected_html)
+        response = self.client.get('/')  
+
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
