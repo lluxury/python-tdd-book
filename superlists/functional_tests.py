39ad0257329bf51ce15807acdb765e7174827811
yann <tmp4321@qq.com>
2018-04-17 18:36:38 +0800

    test Item calss

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -26,6 +26,11 @@ class NewVisitorTest(unittest.TestCase):
     #     rows = table.find_elements_by_tag_name('tr')
     #     self.assertIn(row_text, row.text for row in rows)
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows  = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row_text, [row.text for row in rows]])
+
     def test_can_start_a_list_and_retrieve_it_later(self): 
         # check out it's homepage
         self.browser.get('http://localhost:8000')
@@ -45,19 +50,28 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         # inputbox.send_keys(Keys.Enter)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # self.browser.get_screenshot_as_file('home_page.png')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
         # rows = table.find_element_by_tag_name('tr') 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
         #     f"New to-do item did not appear in table. Contents were:]\n{table.text}"
         # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 
         self.fail('Finish the test!')
