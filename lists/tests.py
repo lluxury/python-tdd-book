c4af51e0f81f3e674245605baf20cebb65da9c0b
yann <tmp4321@qq.com>
2018-04-17 22:52:36 +0800

    Saving the POST to the Database

--- lists/tests.py
+++ lists/tests.py
@@ -24,8 +24,13 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertIn(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
-#        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'home.html')
 #
 #     def test_home_page_returns_correct_html(self):
 #         request = HttpRequest()
