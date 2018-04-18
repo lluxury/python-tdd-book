2e954106aafaa9d6d80b95a7c6ff549ecf014d24
yann <tmp4321@qq.com>
2018-04-18 22:21:36 +0800

    test redirects after POST

--- lists/tests.py
+++ lists/tests.py
@@ -12,60 +12,25 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-#    def test_home_page_returns_correct_html(self):
-#        response = self.client.get('/')  
-#
-#        html = response.content.decode('utf8')  
-#        self.assertTrue(html.startswith('<html>'))
-#        self.assertIn('<title>To-Do lists</title>', html)
-#        self.assertTrue(html.strip().endswith('</html>'))
-#
-#        self.assertTemplateUsed(response, 'home.html')
-
-
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        # self.assertIn('A new list item', response.content.decode())
-        # self.assertTemplateUsed(response, 'home.html')
-
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
-        
-    # def test_can_save_a_POST_request(self):
-    #     response = self.client.post('/', data={'item_text': 'A new list item'})
-
-    #     self.assertEqual(Item.objects.count(), 1)
-    #     new_item = Item.objects.first()
-    #     self.assertIn(new_item.text, 'A new list item')
-
-    #     self.assertIn('A new list item', response.content.decode())
-    #     self.assertTemplateUsed(response, 'home.html')
-
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertEqual(response['location'], '/')
 
     def test_only_saves_item_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-        # self.assertEqual(Item.objects.content(), 0)
-
-#
-#     def test_home_page_returns_correct_html(self):
-#         request = HttpRequest()
-#         response = home_page(request)
-#         html = response.content.decode('utf8')
-#         expected_html = render_to_string('home.html')
-#         self.assertEqual(html, expected_html)
 
 class ItemModelTest(TestCase):
 
-    def test_saving_and_retriving_items(self):
+    def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.save()
