d60bc364d4575f0ccd68f43b6f1e7037c2541867
yann <tmp4321@qq.com>
2018-04-19 23:25:21 +0800

    test multiple user can start lists at different urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,7 +44,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows  = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
+    def test_can_start_a_list_for_one_user(self): 
         # check out it's homepage
         self.browser.get(self.live_server_url)
         # self.browser.get_screenshot_as_file('home_page.png')
@@ -69,18 +69,24 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         # self.browser.get_screenshot_as_file('home_page.png')
 
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # rows = table.find_element_by_tag_name('tr') 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # time.sleep(1)
 
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 
-        self.fail('Finish the test!')
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
+        #She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
