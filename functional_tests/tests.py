058ee51e0790b3adf3a6d2349098eb6abec823b8
yann <tmp4321@qq.com>
2018-04-19 22:09:32 +0800

    use wait func

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,18 +26,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
-    start_time = time.time()
-    while True:
-        try:
-            table = self.browser.find_element_by_id('id_list_table')
-            rows = table.find_elements_by_tag_name('tr')
-            self.assertIn(row_text, [row.text for row in rows])
-            return
-        except (AssertionError, WebDriverException) as e:
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
 
-            if time.time() - start_time > MAX_WAIT:
-                raise e
-            time.sleep(0.5)
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -65,9 +65,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         # inputbox.send_keys(Keys.Enter)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         # self.browser.get_screenshot_as_file('home_page.png')
 
         # table = self.browser.find_element_by_id('id_list_table')
@@ -78,8 +77,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 
         self.fail('Finish the test!')
