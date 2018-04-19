01ee2c685d61a9fb1e172ec2ec4a821a872ba39b
yann <tmp4321@qq.com>
2018-04-19 21:56:12 +0800

    wait for row in list table func

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,8 @@ from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+MAX_WAIT = 10
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
@@ -23,6 +25,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):  
         self.browser.quit()
 
+    def wait_for_row_in_list_table(self, row_text):
+    start_time = time.time()
+    while True:
+        try:
+            table = self.browser.find_element_by_id('id_list_table')
+            rows = table.find_elements_by_tag_name('tr')
+            self.assertIn(row_text, [row.text for row in rows])
+            return
+        except (AssertionError, WebDriverException) as e:
+
+            if time.time() - start_time > MAX_WAIT:
+                raise e
+            time.sleep(0.5)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
