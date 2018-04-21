509a5e335c680a36b94e45a13900cf0707d5e101
yann <tmp4321@qq.com>
2018-04-22 07:42:12 +0800

    existing list url now looks for unique id

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,7 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
+def view_list(request, list_id):
     # pass
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
