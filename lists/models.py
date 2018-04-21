bff3da0c0339aae87186bffe81d526324503d826
yann <tmp4321@qq.com>
2018-04-21 15:00:53 +0800

    naively add list as textfield on item

--- lists/models.py
+++ lists/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.TextField(default='')
 
 # class List(object):
 class List(models.Model):
