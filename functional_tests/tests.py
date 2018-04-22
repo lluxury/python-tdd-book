dd83ae9b9a3ca15ada032494cb63861bf7b0513c
yann <tmp4321@qq.com>
2018-04-22 10:00:34 +0800

    new URL + view for adding to existing lists. FT passes :=)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -111,9 +111,11 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Francis get his own unique URL
-        Francis_list_url = self.browser.current_url
+        francis_list_url = self.browser.current_url
+        # Francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
-        self.assertNotEquaal(francis_list_url, edith_list_url)
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        # self.assertNotEquaal(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
