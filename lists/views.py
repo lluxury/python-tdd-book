140f709cf87379ba088a2b04531b665927923560
yann <tmp4321@qq.com>
2018-04-22 09:43:59 +0800

    view now just passes the list object to template

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,7 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    # items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
